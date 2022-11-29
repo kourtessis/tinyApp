d06a95bcd40a7ab78b4ad24511d9c51d9ac66b4f
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-28 17:34:09 -0800

    URL shortening part 2, edge case handle/ groupwork

--- express_server.js
+++ express_server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = () => {
+  return ((Math.random() + 1)* 0x10000).toString(36).substring(6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,21 +22,45 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const randomName = generateRandomString()
+  const newLongUrl = req.body.longURL
+  if (newLongUrl.slice(0,8) === 'https://' || newLongUrl.slice(0,7) === 'http://') {
+    urlDatabase[randomName] = newLongUrl  // check if contains http: already
+  } else {
+    urlDatabase[randomName] = `https://${newLongUrl}`  // check if contains http: already
+  }
+  res.redirect(`/urls/${randomName}`)
+  console.log(urlDatabase)
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  const templateVars = { id, longURL};
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  console.log(urlDatabase[id])
+  res.redirect(longURL);
+});
