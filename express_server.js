0cdbacd6296ebcac686fbee4ccddd4764e986a9f
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-29 17:29:31 -0800

    add username login and display

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 const generateRandomString = () => {
   return ((Math.random() + 1) * 0x10000).toString(36).substring(6);
@@ -30,7 +32,7 @@ app.listen(PORT, () => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,username: req.cookies["username"], };
   res.render("urls_index", templateVars);
 });
 
@@ -73,12 +75,19 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id/edit", (req, res) => {
   const shortUrl = req.params.id;
-  const newLongUrl = req.body.longUrl
-  
+  const newLongUrl = req.body.longUrl;
+
   if (newLongUrl.slice(0, 8) === 'https://' || newLongUrl.slice(0, 7) === 'http://') {
     urlDatabase[shortUrl] = newLongUrl;  // adds http: into input feild so http not manually required
   } else {
     urlDatabase[shortUrl] = `https://${newLongUrl}`;  // check if contains https: already
   }
+  res.redirect('/urls');
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username) // how to set cookie
+  console.log(req.body)
   res.redirect('/urls');
 });
